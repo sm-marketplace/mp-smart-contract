5f5027e53001ed8a694d2e3f10cfe9f1a83ef053
Roger Ramos <roger.ramos@gdpteam.com>
2022-12-08 23:07:09 -0500

    fix: hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,25 +4,30 @@ require('dotenv-defaults').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
 
+const ACCOUNT_SECRET_KEY = process.env.ACCOUNT_SECRET_KEY || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const GOERLI_URL = process.env.ALCHEMY_API_KEY? 
+  `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
+  'https://rpc.ankr.com/eth_goerli'
+
+
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     localnet: {
-      url: `http://127.0.0.1:3000`, // 
-      accounts: [ '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80' ] // Private key
+      url: `http://127.0.0.1:3000`, 
+      accounts: [ ACCOUNT_SECRET_KEY ]
     },
     hardhat: {
       chainId: 1337
     },
     mumbai: {
-      // Infura
       url: "https://rpc-mumbai.matic.today",
-      accounts: [process.env.ACCOUNT_SECRET_KEY]
+      accounts: [ ACCOUNT_SECRET_KEY ]
     },
     goerli: {
-      // url: "https://rpc.ankr.com/eth_goerli",
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.ACCOUNT_SECRET_KEY]
+      // url: "",
+      url: GOERLI_URL,
+      accounts: [ ACCOUNT_SECRET_KEY ]
     }
     // ...other networks
   },
