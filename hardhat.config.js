f6f71e0444722a3811a56f262c3988c253c7a674
Roger Ramos <roger.ramos@gdpteam.com>
2022-12-17 21:10:11 -0500

    fix env var

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ require('dotenv-defaults').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
 
-const ACCOUNT_SECRET_KEY = process.env.ACCOUNT_SECRET_KEY || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const CONTRACT_OWNER_SECRET = process.env.CONTRACT_OWNER_SECRET || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 const GOERLI_URL = process.env.ALCHEMY_API_KEY? 
   `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` :
   'https://rpc.ankr.com/eth_goerli'
@@ -15,19 +15,19 @@ module.exports = {
   networks: {
     localnet: {
       url: `http://127.0.0.1:3000`, 
-      accounts: [ ACCOUNT_SECRET_KEY ]
+      accounts: [ CONTRACT_OWNER_SECRET ]
     },
     hardhat: {
       chainId: 1337
     },
     mumbai: {
       url: "https://rpc-mumbai.matic.today",
-      accounts: [ ACCOUNT_SECRET_KEY ]
+      accounts: [ CONTRACT_OWNER_SECRET ]
     },
     goerli: {
       // url: "",
       url: GOERLI_URL,
-      accounts: [ ACCOUNT_SECRET_KEY ]
+      accounts: [ CONTRACT_OWNER_SECRET ]
     }
     // ...other networks
   },
